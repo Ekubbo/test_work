8c382b01f52d57c168bddffe805fba97d6dea3e8
Ekubbo <45827702+Ekubbo@users.noreply.github.com>
2019-03-27 00:14:44 +0300

    add models Account, Transaction

---
+++ bonus_account/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+TRANSACTION_TYPE = (
+    ("Pay.", "Payment", ),
+    ("Bon.", "Bonus payment", ),
+    ("Acc", "Bonus accrual", ),
+)
+
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    cart_number = models.IntegerField()
+    balance = models.IntegerField()
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
+class Transaction(models.Model):
+    transaction_type = models.CharField(max_length=20,
+                                        choices=TRANSACTION_TYPE,
+                                        default=TRANSACTION_TYPE[0][0])
+    amount = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self, *args, **kwargs):
+        super(Transaction, self).save(*args, **kwargs)
+
+        if self.transaction_type == TRANSACTION_TYPE[1][0]:
+            self.account.balance = self.account.balance - self.amount
+            self.account.save()
+        elif self.transaction_type == TRANSACTION_TYPE[2][0]:
+            self.account.balance = self.account.balance + self.amount
+            self.account.save()
