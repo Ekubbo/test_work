2dcf4c77f4ef74d8e59d7451c15d68670085801b
Ekubbo <45827702+Ekubbo@users.noreply.github.com>
2019-03-27 10:26:56 +0300

    set __str__ Account

--- bonus_account/models.py
+++ bonus_account/models.py
@@ -20,7 +20,7 @@ class Account(models.Model):
         ordering = ['-id']
 
     def __str__(self):
-        return "{} {}".format(self.first_name, self.last_name)
+        return "{} {}".format(self.last_name, self.first_name)
 
 
 class Transaction(models.Model):
